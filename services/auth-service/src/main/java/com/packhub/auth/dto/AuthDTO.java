83425aa12195878e6ca9a19ac938cf4a1ce652af
Arthur <Arthurfmuniz1@gmail.com>
2025-06-05 16:33:56 -0300

    feat(auth):implementa UserService e atualiza DTO e repository

---
+++ services/auth-service/src/main/java/com/packhub/auth/dto/AuthDTO.java
@@ -0,0 +1,15 @@
+package com.packhub.auth.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthDTO {
+
+    private int userCode;
+    private String senha;
+}
+
+
