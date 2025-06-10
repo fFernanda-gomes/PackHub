41575dc5f27baa62d02caa9829bcc061e25ac274
Willian Duarte <williandasilvaduarte10@gmail.com>
2025-06-09 22:38:30 -0300

    feat(auth): configuração do jwt

---
+++ services/auth-service/src/main/java/com/packhub/auth/config/JwtConfig.java
@@ -0,0 +1,32 @@
+package com.packhub.auth.config;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtConfig {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    private final long EXPIRATION_MS = 1000 * 60 * 60 * 2; // 2h
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateToken(String subject) {
+        return Jwts.builder()
+                .setSubject(subject)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_MS))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
