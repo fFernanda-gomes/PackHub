57ca655afe419ae4c537902bbc4a5ac37dfbd87b
henry <luiz03hamer@gmail.com>
2025-06-12 17:36:40 -0300

    feat(products): criação do endpoint de pegar por code do usuario

--- services/product-service/src/main/java/com/packhub/product/domain/repositories/ProductRepository.java
+++ services/product-service/src/main/java/com/packhub/product/domain/repositories/ProductRepository.java
@@ -3,7 +3,12 @@ package com.packhub.product.domain.repositories;
 import com.packhub.product.domain.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
+
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findAllByUserCode(String userCode);
+
 }
+
