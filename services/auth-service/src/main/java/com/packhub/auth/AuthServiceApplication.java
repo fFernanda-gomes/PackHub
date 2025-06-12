305379ba39d309b7c1967699ec5c4fed8ee19981
fFernanda-gomes <feeh.gomes0110@gmail.com>
2025-06-12 19:40:00 -0300

    feat/create dockerfiles e add to docker-compose

--- services/auth-service/src/main/java/com/packhub/auth/AuthServiceApplication.java
+++ services/auth-service/src/main/java/com/packhub/auth/AuthServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
-		EnvLoader.load();
 		SpringApplication.run(AuthServiceApplication.class, args);
 	}
 
