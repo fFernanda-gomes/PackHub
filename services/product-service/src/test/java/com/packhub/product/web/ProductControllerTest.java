03683a23907a88da1850b99f43a97af006334832
fFernanda-gomes <feeh.gomes0110@gmail.com>
2025-06-12 21:20:46 -0300

    feat/config-junit

---
+++ services/product-service/src/test/java/com/packhub/product/web/ProductControllerTest.java
@@ -0,0 +1,22 @@
+package com.packhub.product.web;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.packhub.product.domain.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@AutoConfigureMockMvc(addFilters = false)
+@WebMvcTest(ProductController.class)
+public class ProductControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ProductService productService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+}
