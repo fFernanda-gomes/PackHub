dd06aa50be53fad55c2e11fd083fe8111b04914a
fFernanda-gomes <feeh.gomes0110@gmail.com>
2025-06-12 21:49:08 -0300

    feat/config-junit

---
+++ services/auth-service/src/test/java/com/packhub/auth/web/UserControllerTest.java
@@ -0,0 +1,22 @@
+package com.packhub.auth.web;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.packhub.auth.domain.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@AutoConfigureMockMvc(addFilters = false)
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+}
