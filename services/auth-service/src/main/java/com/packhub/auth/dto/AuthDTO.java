8518f6aa5e6ae002744e41d02631c2281f943ca8
fFernanda-gomes <feeh.gomes0110@gmail.com>
2025-06-13 21:55:14 -0300

    docs(auth): melhorias no documentação dos endpoints de autenticação e usuário no Swagger

--- services/auth-service/src/main/java/com/packhub/auth/dto/AuthDTO.java
+++ services/auth-service/src/main/java/com/packhub/auth/dto/AuthDTO.java
@@ -1,5 +1,7 @@
 package com.packhub.auth.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @NoArgsConstructor
@@ -9,8 +11,14 @@ import lombok.*;
 @Builder
 public class AuthDTO {
 
+    @Schema(description = "Código do usuário", example = "1111")
+    @NotBlank(message = "userCode é obrigatório")
     private Integer userCode;
+
+    @Schema(description = "Senha do usuário", example = "senha123")
+    @NotBlank(message = "password é obrigatória")
     private String password;
+
     private Long id;
     private String token;
 }
